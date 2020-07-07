8862ff11fed7b37df1b23f16a41266a0dfb672a8
coderaky <45727674+coderaky@users.noreply.github.com>
2020-07-07 16:00:47 +0530

    added 7 tone

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,61 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playsound(int soundno) {
+    final player = AudioCache();
+    player.play('note$soundno.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Press Me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playsound(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playsound(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playsound(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playsound(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playsound(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.indigo,
+                onPressed: () {
+                  playsound(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.blueGrey,
+                onPressed: () {
+                  playsound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
